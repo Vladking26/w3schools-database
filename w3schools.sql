66bbfdb9f15ce21b113ac475cce7e185f892d96c
Vladking26 <ndemndemvladimir@gmail.com>
2026-01-09 11:51:55 +0200

    Rename database from 'w3schools' to 'Sql_Practice'

--- w3schools.sql
+++ w3schools.sql
@@ -10,10 +10,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
-CREATE DATABASE IF NOT EXISTS `w3schools` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `w3schools`;
+CREATE DATABASE IF NOT EXISTS `Sql_Practice` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `Sql_Practice`;
 --
--- Database: `w3schools`
+-- Database: `Sql_Practice`
 --
 
 -- --------------------------------------------------------
